a18bff1f1f3cd3aaa487f438fb2bdac3a9b38e96
iva mudronja <iva.pilizota@gmail.com>
2020-09-13 23:50:35 +0200

    created chat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Chatroom.create(name: "general")
+User.create(email: "ilona@lewagon.org", nickname: "ilona", password: "123456")
+User.create(email: "alexis@lewagon.org", nickname: "alexis", password: "123456")
+User.create(email: "patrik@lewagon.org", nickname: "patrik", password: "123456")
+User.create(email: "iva@lewagon.org", nickname: "iva", password: "123456")
+
+puts 'done :)'
