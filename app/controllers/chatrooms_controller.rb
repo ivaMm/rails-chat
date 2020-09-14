0747dd6fa3ae20ae31d5e66027228d3a02a7e7b0
iva mudronja <iva.pilizota@gmail.com>
2020-09-14 15:21:20 +0200

    updated seeds, added multiple chatrooms, front - wip

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,6 +2,7 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
-    # @chatrooms = Chatroom.all
+    @chatrooms = Chatroom.all
+    @users = User.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Chatroom.destroy_all
+User.destroy_all
+
+Chatroom.create(name: "batch-386")
 Chatroom.create(name: "general")
-User.create(email: "ilona@lewagon.org", nickname: "ilona", password: "123456")
-User.create(email: "alexis@lewagon.org", nickname: "alexis", password: "123456")
-User.create(email: "patrik@lewagon.org", nickname: "patrik", password: "123456")
-User.create(email: "iva@lewagon.org", nickname: "iva", password: "123456")
+Chatroom.create(name: "berlin")
+Chatroom.create(name: "career")
+Chatroom.create(name: "design")
+Chatroom.create(name: "help")
+Chatroom.create(name: "freelancers")
+Chatroom.create(name: "ruby")
+Chatroom.create(name: "jobs")
+Chatroom.create(name: "javascript")
+Chatroom.create(name: "product")
+Chatroom.create(name: "resources")
+
+User.create(email: "ilona@lewagon.org", nickname: "Ilona", password: "123456")
+User.create(email: "alexis@lewagon.org", nickname: "Alexis", password: "123456")
+User.create(email: "patrik@lewagon.org", nickname: "Patrik", password: "123456")
+User.create(email: "ryan@lewagon.org", nickname: "Ryan", password: "123456")
+User.create(email: "toni@lewagon.org", nickname: "Toni", password: "123456")
+User.create(email: "wiebke@lewagon.org", nickname: "Wiebke", password: "123456")
+User.create(email: "jadwiga@lewagon.org", nickname: "Jadwiga", password: "123456")
+User.create(email: "assunta@lewagon.org", nickname: "Assunta", password: "123456")
+User.create(email: "iva@lewagon.org", nickname: "Iva", password: "123456")
 
 puts 'done :)'
