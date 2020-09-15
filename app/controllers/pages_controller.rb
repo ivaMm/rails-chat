0a624e953be782a8ee35ddcbebcd236f966e6a29
iva mudronja <iva.pilizota@gmail.com>
2020-09-15 23:23:49 +0200

    wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if current_user
+      @chatroom = Chatroom.first
+      redirect_to chatroom_path(@chatroom)
+    end
   end
 end
