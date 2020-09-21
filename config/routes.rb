129cad5640687392ad4d9d594afb066acf23f742
iva mudronja <iva.pilizota@gmail.com>
2020-09-21 23:04:27 +0200

    added simple search

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :chatrooms, only: [:new, :create, :show] do
-    resources :messages, only: :create
+    resources :messages, only: [:index, :create]
   end
 end
