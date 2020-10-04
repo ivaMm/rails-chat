8508f163d869d576c8d885271f1f1e9ce3902787
iva mudronja <iva.pilizota@gmail.com>
2020-10-04 14:24:13 +0200

    changed content to be string in PrivateMessages, improved new_private_message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_192456) do
+ActiveRecord::Schema.define(version: 2020_10_04_121109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2020_10_03_192456) do
   end
 
   create_table "private_messages", force: :cascade do |t|
-    t.text "content"
+    t.string "content"
     t.bigint "user_id", null: false
     t.bigint "conversation_id", null: false
     t.datetime "created_at", precision: 6, null: false
