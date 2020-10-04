c675da3bd511a145c0ee4f7bf80803718aa82324
iva mudronja <iva.pilizota@gmail.com>
2020-10-04 17:11:37 +0200

    improved order

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -17,7 +17,7 @@ class ChatroomsController < ApplicationController
       @chatrooms = Chatroom.all.order(created_at: :desc)
       @users = User.all.where.not(id: current_user.id)
       @messages = Message.order(created_at: :desc).search_by_content(@query)
-      @private_messages = PrivateMessage.includes(current_user).search_by_content(@query)
+      @private_messages = PrivateMessage.includes(current_user).order(created_at: :desc).search_by_content(@query)
     end
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -12,7 +12,7 @@ class ConversationsController < ApplicationController
       @chatrooms = Chatroom.all.order(created_at: :desc)
       @users = User.all.where.not(id: current_user.id)
       @messages = Message.order(created_at: :desc).search_by_content(@query)
-      @private_messages = PrivateMessage.includes(current_user).search_by_content(@query)
+      @private_messages = PrivateMessage.includes(current_user).order(created_at: :desc).search_by_content(@query)
     end
     @chatroom = Chatroom.last
     @conversation = Conversation.find(params[:id])
